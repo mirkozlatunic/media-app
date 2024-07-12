8dd162d96d71b1af37cb2b15ec45e7b9c7f94271
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-07-11 22:19:21 -0700

    tag implemetation

--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -21,7 +21,7 @@ const albumsApi = createApi({
     return {
       removeAlbum: builder.mutation({
         invalidatesTags: (results, error, album) => {
-          return [{ type: "Album", id: album.userId }];
+          return [{ type: "Album", id: album.id }];
         },
         query: (album) => {
           return {
@@ -47,7 +47,11 @@ const albumsApi = createApi({
       }),
       fetchAlbums: builder.query({
         providesTags: (result, error, user) => {
-          return [{ type: "Album", id: user.id }];
+          const tags = result.map((album) => {
+            return { type: "Album", id: album.id };
+          });
+          tags.push({ type: "UsersAlbums", id: user.id });
+          return tags;
         },
         query: (user) => {
           return {
