f2a045b886d098b63f3e0f16cc09571432a96eb2
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-07-12 23:20:28 -0700

    adding endpoints

---
+++ src/components/PhotosList.js
@@ -0,0 +1,8 @@
+import { useFetchPhotosQuery } from "../store";
+
+function PhotosList({ album }) {
+  useFetchPhotosQuery(album);
+  return "PhotoList";
+}
+
+export default PhotosList;
---
+++ src/components/PhotosListItem.js
@@ -0,0 +1,5 @@
+function PhotoListItem() {
+  return "PhotoListItem";
+}
+
+export default PhotoListItem;
